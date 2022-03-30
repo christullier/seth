6ae43ecd4d085d19cb626fdc9be25d295f6b0e90
CamJak <cameron102601@gmail.com>
2022-03-30 18:40:36 -0500

    Added assignment 4

---
+++ Assignment 4 - Covert Chat/covertChat_cam/chat-client.py
@@ -0,0 +1,61 @@
+from socket import socket, AF_INET, SOCK_STREAM
+from time import time
+from sys import stdout
+
+DEBUG = False
+
+# set timings for bits
+ZERO = 0.025
+ONE = 0.1
+
+
+# declare ip and port to connect to
+ip = "localhost"
+port = 1337
+
+# initialize the socket
+s = socket(AF_INET, SOCK_STREAM)
+
+# connect to server
+s.connect((ip, port))
+
+# recieve data from server
+data = s.recv(4096).decode()
+
+cov_bin = ""
+# print data until EOF is recieved
+while ((data.rstrip("\n")) != "EOF"):
+	stdout.write(data)
+	stdout.flush()
+	
+	t0 = time()
+	data = s.recv(4096).decode()
+	t1 = time()
+	
+	delta = round(t1 - t0, 3)
+	
+	if DEBUG:
+		stdout.write(f" {delta}\n")
+		stdout.flush
+		
+	if delta >= ONE:
+		cov_bin += "1"
+	else:
+		cov_bin += "0"
+
+# close connection to server
+s.close()
+
+
+covert = ""
+i = 0
+while (i < len(cov_bin)):
+	b = cov_bin[i:i+8]
+	try:
+		covert += chr(int(f"0b{b}", 2))
+	except:
+		covert += "?"
+	i += 8
+
+print(covert)
+
---
+++ Assignment 4 - Covert Chat/covertChat_cam/chat-server.py
@@ -0,0 +1,56 @@
+from socket import socket, AF_INET, SOCK_STREAM
+from time import sleep
+
+
+# set timings for bits
+ZERO = 0.025
+ONE = 0.1
+
+
+# set port for server
+ip = ""
+port = 1337
+
+# create socket and bind to port
+s = socket(AF_INET, SOCK_STREAM)
+s.bind((ip, port))
+
+# set server to listen for connections
+s.listen(0)
+print("Server is listening...")
+
+# save socket of connection to c
+c, addr = s.accept()
+
+
+# define overt message to be sent
+msg = "BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH BLAH\n"
+
+# define covert message to be encoded
+msg_c = "Secret message" + "EOF"
+msg_c_bin = ""
+
+for char in msg_c:
+	char_bin = bin(ord(char))[2:].zfill(8)
+	msg_c_bin += char_bin
+
+# send characters of message
+n=0
+for i in msg:
+	# send overt character to client
+	c.send(i.encode())
+	
+	# change sleep depending on bits to encode
+	if msg_c_bin[n] == "0":
+		sleep(ZERO)
+	else:
+		sleep (ONE)
+	
+	# increment n but stay within bounds of covert message length
+	n = (n+1) % len(msg_c_bin)
+
+# send EOF to finish message
+c.send("EOF".encode())
+print("Message sent...")
+c.close()
+
