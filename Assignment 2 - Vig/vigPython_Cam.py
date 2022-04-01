db14a7770350d3701b6cca98d8eba0760167574c
CamJak <cameron102601@gmail.com>
2022-04-01 11:33:43 -0500

    Added challenge folder

---
+++ Assignment 2 - Vig/vigPython_Cam.py
@@ -0,0 +1,30 @@
+
+def decode():
+	# decode here 
+	print(bruh)
+    
+
+def encode():
+	# encode here
+    print(hetlp)
+
+
+def main():
+	# main body here
+	message = "howdoesthiswork"
+	lenMsg = len(message)
+	key = "key"
+    lenKey = len(key)
+    
+    i=0
+    key_cnt=0
+    
+	while (i<lenMsg):
+        char1 = message[i:1]
+        print(char1)
+        
+        i++
+        key_cnt++
+
+
+main()
