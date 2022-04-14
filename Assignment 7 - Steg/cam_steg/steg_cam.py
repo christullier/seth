0c0eb0127bef9b1a7d226039c68984e9d3a0d598
CamJak <cameron102601@gmail.com>
2022-04-14 13:06:45 -0500

    Mostly finished steg

--- Assignment 7 - Steg/cam_steg/steg_cam.py
+++ Assignment 7 - Steg/cam_steg/steg_cam.py
@@ -43,7 +43,7 @@ if byte:
 	wrapme = bytearray(f.read())
 	f.close()
 	
-	if store:
+	if store == True:
 		f = open(secret, 'rb')
 		hideme = bytearray(f.read())
 		f.close()
@@ -52,7 +52,7 @@ if byte:
 	wSize = os.path.getsize(f"./{wrapper}")
 	hSize = os.path.getsize(f"./{secret}")
 	
-	if store:
+	if store == True:
 		## STORAGE
 
 		# run loop to hide bytes in wrapper
@@ -68,7 +68,7 @@ if byte:
 
 		sys.stdout.buffer.write(wrapme)
 	
-	elif retrieve:
+	elif retrieve ==  True:
 		## EXTRACTION
 		sent_found = False
 		empty = bytearray()
@@ -88,7 +88,7 @@ if byte:
 				offset += interval
 				sent_pos = True
 				# while a SENTINEL is possible and not found yet
-				while ((sent_pos) and (sent_found == False)):
+				while ((sent_pos == True) and (sent_found == False)):
 					b[0] = wrapme[offset]
 					# if b matches the next SENTINEL byte, continue search
 					if (b[0] == SENTINEL[sent_i]):
@@ -102,7 +102,6 @@ if byte:
 					# if possible SENTINEL equals SENTINEL set it to found, otherwise...
 					if (pos_sent_buff == SENTINEL):
 						sent_found = True
-						break
 						
 			# add byte to hidden file and increase index
 			if not sent_found:				
@@ -113,16 +112,98 @@ if byte:
 		sys.stdout.buffer.write(empty)
 
 
-elif bit:
+elif bit == True:
 	### BIT METHOD
-	if store:
+	
+	# set sentinel value for bytes
+	SENTINEL = bytearray("EOF", "UTF-8")
+	
+	# read in bytes from files
+	f = open(wrapper, 'rb')
+	wrapme = bytearray(f.read())
+	f.close()
+	
+	if store == True:
+		f = open(secret, 'rb')
+		hideme = bytearray(f.read())
+		f.close()
+	
+	if store == True:
 		## STORAGE
-		placeholder = ""
+		for i in range(0, len(hideme)):
+			for j in range(0, 8):
+				wrapme[offset] &= 0b11111110
+				wrapme[offset] |= ((hideme[i] & 0b10000000) >> 7)
+				hideme[i] = (hideme[i] << 1) & (2 ** 8 - 1)
+				offset += interval
+			i += 1
+		
+		for i in range(0, len(SENTINEL)):
+			for j in range(0, 8):
+				wrapme[offset] &= 0b11111110
+				wrapme[offset] |= ((SENTINEL[i] & 0b10000000) >> 7)
+				SENTINEL[i] = (SENTINEL[i] << 1) & (2 ** 8 - 1)
+				offset += interval
+			i += 1
+		
+		sys.stdout.buffer.write(wrapme)
 
 
 	elif retrieve:
 		## EXTRACTION
-		nothinghere = ""
-
-
+		sent_found = False
+		empty = bytearray()
+		pos_sent_buff = bytearray()
+		b = bytearray(1)
+		while (offset < len(wrapme) and sent_found == False):
+			b[0] = 0
+			
+			# create a byte out of bits taken from wrapper
+			for j in range(0, 8):
+				b[0] |= (wrapme[offset] & 0b00000001)
+				if (j < 7):
+					b[0] = (b[0] << 1) & (2 ** 8 - 1)
+					offset += interval
+			
+			# if b matches first SENTINEL byte and SENTINEL not found
+			if ((b[0] == SENTINEL[0]) and (sent_found == False)):
+				# create a buffer for possible SENTINEL bytes
+				# set SENTINEL index
+				# increment offset
+				# set possibility of SENTINEL to true
+				pos_sent_buff.extend(b)
+				sent_i = 1
+				offset += interval
+				sent_pos = True
+				# while a SENTINEL is possible and not found yet
+				while ((sent_pos == True) and (sent_found == False)):
+					b[0] = 0
+					
+					# create a byte out of bits taken from wrapper
+					for j in range(0, 8):
+						b[0] |= (wrapme[offset] & 0b00000001)
+						if (j < 7):
+							b[0] = (b[0] << 1) & (2 ** 8 - 1)
+							offset += interval
+					
+					# if b matches the next SENTINEL byte, continue search
+					if (b[0] == SENTINEL[sent_i]):
+						pos_sent_buff.extend(b)
+						offset += interval
+						sent_i += 1
+					# else if it does not match, break off search for now and add buffer to file
+					elif (b[0] != SENTINEL[sent_i]):
+						sent_pos = False
+						empty.extend(pos_sent_buff)
+					# if possible SENTINEL equals SENTINEL set it to found
+					if (pos_sent_buff == SENTINEL):
+						sent_found = True
+						
+			# add byte to hidden file and increase index
+			if not sent_found:				
+				empty.extend(b)
+				offset += interval
+		
+		# write to stdout
+		sys.stdout.buffer.write(empty)
 
