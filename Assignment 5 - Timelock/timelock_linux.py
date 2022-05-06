15203e10212b22dc339fe673858a4f202c39e7b1
CamJak <cameron102601@gmail.com>
2022-05-06 10:43:11 -0500

    Moved final versions to challenge folder

--- Assignment 5 - Timelock/timelock_linux.py
+++ Assignment 5 - Timelock/timelock_linux.py
@@ -1,14 +1,13 @@
+# imports
 import sys
 import re
 import backports.zoneinfo as zoneinfo
 from hashlib import md5
-from datetime import datetime, timedelta
+from datetime import datetime
 from tzlocal import get_localzone
 from pytz import timezone
-import time
 
-
-# take stdin as input for epoch
+# get epoch from user via stdin
 user_in = sys.stdin.readlines()
 epoch = user_in[0][:-1]
 epoch = epoch.split(" ")
@@ -17,41 +16,42 @@ epoch = epoch.split(" ")
 local = get_localzone()
 
 # format
-formats = "%Y %m %d %H %M %S"
+format = "%Y %m %d %H %M %S"
 
-## get current time
-now = datetime(2013, 5, 6, 7, 43, 25) # was 25
-time_cur = now.strftime(formats).split(" ")
+# get current time (uncomment first line to use system time)
+# now = datetime.now(local)
+now = datetime(2017, 3, 23, 18, 2, 6)
+time_cur = now.strftime(format).split(" ")
 
-# something something comment
+# string representations of above lists for
+# current and epoch times
 epocStr = " "
-timeCurStr = " "
-
-# blah blah blha blahh
 epocStr = epocStr.join(epoch)
+timeCurStr = " "
 timeCurStr = timeCurStr.join(time_cur)
 
-# datetime objects
-d1 = datetime.strptime(epocStr, formats)
-d2 = datetime.strptime(timeCurStr, formats)
+# date-time objects
+d1 = datetime.strptime(epocStr, format)
+d2 = datetime.strptime(timeCurStr, format)
 
-# calc seconds elapsed
+# calculate number of seconds elapsed
 days_elapsed = abs(d2 - d1).days
 seconds_elapsed = abs(d2 - d1).seconds
 seconds_elapsed += (days_elapsed * 86400)
 
-# check for daylight savings
-dst_date2 = datetime(2013, 5, 6, 7, 43, 25, tzinfo=zoneinfo.ZoneInfo(key='US/Central'))
-if (dst_date2.dst()):
+# check for dst
+dst_date2 = datetime(2017, 3, 23, 18, 2, 6, tzinfo = zoneinfo.ZoneInfo(key = 'US/Central'))
+if(dst_date2.dst()):
 	seconds_elapsed -= 3600
 
+# find distance to beginning of 60-second interval
 modQuotient = seconds_elapsed%60
 
 # create hash using MD5
 hash1 = md5(str(seconds_elapsed-modQuotient).encode())
-hash1000 = hash1.hexdigest()
-hash2 = md5(str(hash1000).encode())
-time_hash = hash2.hexdigest()
+time_hash = hash1.hexdigest()
+hash1 = md5(str(time_hash).encode())
+time_hash = hash1.hexdigest()
 time_hash_rev = time_hash[::-1]
 
 # use regex to get code
@@ -63,4 +63,3 @@ code = match1[0] + match1[1] + match2[0] + match2[1]
 print("Time elapsed: " + str(seconds_elapsed))
 print("Hash: " + time_hash)
 print("Code: " + code)
-
