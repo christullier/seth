7f88a3fcb7599791f06a6bc078a96007db8a6b2f
CamJak <cameron102601@gmail.com>
2022-05-11 11:41:22 -0500

    Files

---
+++ Challenge 6/xor.py
@@ -0,0 +1,25 @@
+#####################################################################################################################
+#                                                                                                                   #
+#       Program 6: XOR Crypto	                                                                           		    #
+#       Group Name: Seth                                                                                            #
+#       Group Members:  Ethan Clapp, Cameron Thomas, Dylan Weaver, Ghufran Aldawood,                                #
+#                       Chris Tullier, David Mains, Will Shepherd                                                   #
+#                                                                                                                   #
+#####################################################################################################################
+
+# imports
+import sys
+
+# get ciphertext from stdin
+user_in = sys.stdin.buffer.read()
+
+# get key
+f = open('k3y', 'rb')
+key = f.read()
+f.close()
+
+# xor byte conversion
+conversion = bytes([_a ^ _b for _a, _b in zip(user_in, key)])
+
+# output conversion to user
+sys.stdout.buffer.write(conversion)
